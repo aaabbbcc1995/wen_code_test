c3ec13f25ae7472a00e512bb1869b431fcb5e4dc
wen <wenzaiquan1995@gmail.com>
2023-11-25 19:40:43 +0800

    feature: basic structure

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,39 @@
-import { Link } from 'react-router-dom';
-
-import logo from '../logo.svg';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 function Home() {
+  const [editStatus, setEditStatus] = useState(false);
+
   return (
-    <header className="app-header">
-      <img src={logo} className="app-logo" alt="logo" />
-      <pre style={{ textAlign: 'left' }}>
-        <code>window.blocklet = {JSON.stringify(window.blocklet, null, 2)}</code>
-      </pre>
-      <Link className="app-link" to="/about">
-        About
-      </Link>
-      <a className="app-link" href="https://developer.blocklet.io/docs/" target="_blank" rel="noopener noreferrer">
-        Learn Blocklet
-      </a>
-    </header>
+    <div className="home-page-container">
+      <div className="home-page-container-header">
+        <div className="avatar" />
+        <div className="username">{localStorage.getItem('username') || 'username'}</div>
+      </div>
+      <div className="home-page-container-content">
+        <RowContent title="Age" />
+        <RowContent title="Gender" />
+        <RowContent title="Email" />
+        <RowContent title="Phone Number" />
+      </div>
+      <div className="home-page-container-button" onClick={() => setEditStatus(!editStatus)}>
+        {editStatus ? 'Save' : 'Edit'}
+      </div>
+    </div>
   );
 }
 
 export default Home;
+
+function RowContent(props) {
+  const { title } = props;
+  return (
+    <div className="home-page-container-row-content">
+      {title}: <div className="home-page-container-row-content-value">{localStorage.getItem(title) || 'Not Set'} </div>
+    </div>
+  );
+}
+
+RowContent.propTypes = {
+  title: PropTypes.string.isRequired,
+};
